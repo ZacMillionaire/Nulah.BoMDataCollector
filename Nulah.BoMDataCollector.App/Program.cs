2b157902ad1524c7f6b01d50365f8c4ad654624e
Scott <scott.schultz@jotunga.com>
2024-01-09 19:13:30 +1000

    Add nullables to any non-nullable value types and add air temp

--- Nulah.BoMDataCollector.App/Program.cs
+++ Nulah.BoMDataCollector.App/Program.cs
@@ -92,10 +92,14 @@ internal class BoMCollector
 				var measurementTime = DateTime.ParseExact(observation.aifstime_utc, "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
 
 				// rain trace can also be a single dash in the response, but lets treat it as if it could be any invalid decimal
-				// value instead and use a TryParse
+				// value instead and use a TryParse.
+				// A single dash is not the same as a zero value for our use case (the data may come back as 0.0 as there
+				// could have been a trace amount of rain somewhere but the json is only accurate to 1 decimal place
+				//http://www.bom.gov.au/catalogue/observations/about-weather-observations.shtml
 				var rainTraceParsed = decimal.TryParse(observation.rain_trace, out var rainTrace);
 
 				var point = PointData.Measurement("weather-data").Tag("source", "Bureau of Meteorology")
+					.Field("air_temp", observation.air_temp)
 					.Field("apparent_t", observation.apparent_t)
 					// this is also known as wet bulb
 					.Field("delta_t", observation.delta_t)
@@ -161,32 +165,32 @@ internal class BoMCollector
 
 	private class Data
 	{
-		public int sort_order { get; set; }
-		public int wmo { get; set; }
+		public int? sort_order { get; set; }
+		public int? wmo { get; set; }
 		public string name { get; set; }
 		public string history_product { get; set; }
 		public string local_date_time { get; set; }
 		public string local_date_time_full { get; set; }
 		public string aifstime_utc { get; set; }
-		public double lat { get; set; }
-		public double lon { get; set; }
-		public double apparent_t { get; set; }
+		public double? lat { get; set; }
+		public double? lon { get; set; }
+		public double? apparent_t { get; set; }
 		public string cloud { get; set; }
 		public object cloud_base_m { get; set; }
 		public object cloud_oktas { get; set; }
 		public object cloud_type_id { get; set; }
 		public string cloud_type { get; set; }
-		public double delta_t { get; set; }
-		public int gust_kmh { get; set; }
-		public int gust_kt { get; set; }
-		public double air_temp { get; set; }
-		public double dewpt { get; set; }
-		public double press { get; set; }
-		public double press_qnh { get; set; }
-		public double press_msl { get; set; }
+		public double? delta_t { get; set; }
+		public int? gust_kmh { get; set; }
+		public int? gust_kt { get; set; }
+		public double? air_temp { get; set; }
+		public double? dewpt { get; set; }
+		public double? press { get; set; }
+		public double? press_qnh { get; set; }
+		public double? press_msl { get; set; }
 		public string press_tend { get; set; }
 		public string rain_trace { get; set; }
-		public int rel_hum { get; set; }
+		public int? rel_hum { get; set; }
 		public string sea_state { get; set; }
 		public string swell_dir_worded { get; set; }
 		public object swell_height { get; set; }
@@ -194,8 +198,8 @@ internal class BoMCollector
 		public string vis_km { get; set; }
 		public string weather { get; set; }
 		public string wind_dir { get; set; }
-		public int wind_spd_kmh { get; set; }
-		public int wind_spd_kt { get; set; }
+		public int? wind_spd_kmh { get; set; }
+		public int? wind_spd_kt { get; set; }
 	}
 
 	#endregion
